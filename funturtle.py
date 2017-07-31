286375c8d8f7d9c6445fb6d000e911f4429798ed
tamara19-meet <tamara19@meet.mit.edu>
2017-07-31 17:21:19 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,33 @@
+import turtle
+turtle.shape('turtle')
+square = turtle.clone()
+square.shape('square')
+square.goto(100,100)
+square.goto(300,300)
+square.stamp()
+square.goto(100,100)
+
+UP = 0
+LEFT = 1
+Down = 2
+Right = 3
+direction = 'UP'
+UP_ARROW = "Up"
+LEFT_ARROW = "Left"
+DOWN_ARROW = "Down"
+RIGHT_ARROW = "Right"
+SPACEBAR = "space"
+
+UP = 0
+
+
+
+
+
+
+
+
+
+
+
+
